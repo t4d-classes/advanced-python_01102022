9748701bb4023424395d62ffbaa8a7d2a3e46609
Eric Greene <eric@t4d.io>
2022-01-10 14:55:47 -0500

    add exercise 1

---
+++ python_demos/dates_demo/time_deltas.py
@@ -0,0 +1,21 @@
+""" time delta demo """
+
+from datetime import datetime, timedelta
+import time
+
+
+start = datetime.now()
+
+end = start + timedelta(days=20)
+
+print(start)
+print(end)
+
+start = datetime.now()
+
+time.sleep(2)
+
+end = datetime.now()
+
+print(end - start)
+print(type(end - start))
---
+++ python_demos/rates_demo/__main__.py
@@ -0,0 +1,4 @@
+""" hello world app """
+
+if __name__ == "__main__":
+    print("Hello, World!")
